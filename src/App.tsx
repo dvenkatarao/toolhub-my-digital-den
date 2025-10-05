65b88edfb0f1fae453302cad24f76bae4f273aa6
dvenkatarao <147297992+dvenkatarao@users.noreply.github.com>
2025-10-05 04:04:31 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ import TempEmail from "./pages/tools/TempEmail";
 import VerifiedEmails from "./pages/tools/VerifiedEmails";
 import Calculators from "./pages/tools/Calculators";
 import ClocksTimers from "./pages/tools/ClocksTimer";
-import AuraFlow from "./pages/tools/ClocksTimer";
+import AuraFlow from "./pages/tools/AuraFlow1";
 import ToolPlaceholder from "./pages/tools/ToolPlaceholder";
 import { Mail, Shield, FileImage, FileText, Lock, Cloud, FolderOpen, Image as ImageIcon, Calculator, Settings, Clock } from "lucide-react";
 import Subscription from "./pages/Subscription";
