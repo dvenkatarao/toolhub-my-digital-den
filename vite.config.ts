bad682f23a39a63d07e12885bd066473d8429e04
dvenkatarao <147297992+dvenkatarao@users.noreply.github.com>
2025-10-06 22:28:12 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     rollupOptions: {
       output: {
         manualChunks: {
-          fflate: ['fflate'],
+          fflate: ['fflate', 'fflate/browser'],
         },
       },
     },
